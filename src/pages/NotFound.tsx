a2d7b610cbeb94d87b00383e1bfd912790dd5ab0
MaxDelissen <maxdelissen17@gmail.com>
2025-04-25 09:44:34 +0200

    Filled in notfound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,21 +1,25 @@
 import { Container, Row, Col, Button } from "react-bootstrap";
+import { t } from "i18next";
 
 function NotFound() {
   return (
-    <Container className="text-center mt-5">
-      <Row>
-        <Col>
-          <h1 className="display-1 text-danger">404</h1>
-          <h2 className="mb-4">Page Not Found</h2>
-          <p className="mb-4">
-            The page you are looking for does not exist or has been moved.
-          </p>
-          <Button variant="primary" href="/">
-            Go to Homepage
-          </Button>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <title>{t("not_found")}</title>
+      <div className="bg-light vh-100 d-flex align-items-center justify-content-center">
+        <Container className="text-center mt-5">
+          <Row>
+            <Col>
+              <h1 className="display-1 text-danger">404</h1>
+              <h2 className="mb-4">{t("not_found")}</h2>
+              <p className="mb-4">{t("not_found_description")}</p>
+              <Button variant="warning" href="/">
+                {t("go_home")}
+              </Button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
   );
 }
 
